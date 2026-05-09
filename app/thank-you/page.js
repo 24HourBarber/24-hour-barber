d3641e8ff2eecdae24012f384e18767ae9d1d471
24HourBarber <24hourbarber@gmail.com>
2026-05-09 16:11:43 -0400

    Create page.js

---
+++ app/thank-you/page.js
@@ -0,0 +1,22 @@
+export default function ThankYouPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#0b0b0d",
+        color: "white",
+        padding: "80px 24px",
+      }}
+    >
+      <h1>Thank you.</h1>
+
+      <p>
+        Your inquiry was received. 24 Hour Barber will follow up soon.
+      </p>
+
+      <a href="/" style={{ color: "#fcd34d" }}>
+        Return home
+      </a>
+    </main>
+  );
+}
